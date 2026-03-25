e8112f907bbf26670d673003610c7bb6a345ae15
Sakshidhore <sakshidhore110@gmail.com>
2026-03-25 17:41:44 +0530

    Refund Distribution by Product for goal3

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@
 import calendar
 import streamlit as st
 import pandas as pd
-import visualization
+import visualization as vis
 
 st.set_page_config(page_title="Analytics Dashboard", layout='wide')
 
@@ -28,10 +28,11 @@ def load_data(path):
  ) = load_data('./data/')
 
 
-st.dataframe(order_item_refunds)
+# st.dataframe(order_item_refunds)
 
 # Setting Title
 st.title("Dashboard")
+
 
 # Module Wise Analysis
 
@@ -54,10 +55,44 @@ with tab[1]:
 
 with tab[2]:
     st.header('Product Performance & Refund Analysis')
+    a,b,c,d=st.columns(4)
+    #KPI TOTAL NET REVENUE
+    merged = pd.merge(order_items,order_item_refunds,on='order_item_id',how='left')
+    sales_per_product = order_items.groupby('product_id')['price_usd'].sum().reset_index()
+    sales_per_product.columns = ['product_id', 'sales_revenue']
+    refund_per_product = merged.groupby('product_id')['refund_amount_usd'].sum().reset_index()
+    prod_info=pd.merge(sales_per_product,refund_per_product,on='product_id')
+    
+    prod_info['net_revenue'] = prod_info['sales_revenue'] - prod_info['refund_amount_usd']
+    total_revenue = prod_info['net_revenue'].sum()
+    a.metric( label="Total Net Revenue",value=f'{total_revenue:,.2f}')
+
+    refund_amt=order_item_refunds["refund_amount_usd"].sum()
+    b.metric( label="Refund Amount",value=f'{refund_amt:,.2f}')
+
+    average_order_value = orders["price_usd"].mean()
+    c.metric( label="Average Order Value",value=f'{average_order_value:,.2f}')
+
+    total_orders=order_items.order_item_id.nunique()
+    d.metric( label="Total Orders",value=f'{total_orders:,.2f}')
+   
+    
+    
+    #REFUND AMOUNT PER PRODUCT
+    merged = pd.merge(order_items,order_item_refunds,on='order_item_id',how='left')
+    sales_per_product = order_items.groupby('product_id')['price_usd'].sum().reset_index()
+    sales_per_product.columns = ['product_id', 'sales_revenue']
+    refund_per_product = merged.groupby('product_id')['refund_amount_usd'].sum().reset_index()
+    products_refund=pd.merge(sales_per_product,refund_per_product,on='product_id')
 
 
-
-with tab[3]:
+    tab2_columns = st.columns([0.7, 0.3], gap='large')
+    
+with tab2_columns[0]:
+        
+ with tab2_columns[1]:
+    st.plotly_chart(vis.plot_refund_distribution(products_refund))
+       
+ with tab[3]:
     st.header('Customer Lifecycle & Repeat Behaviour')
-
-
+    
--- visualization.py
+++ visualization.py
@@ -2,3 +2,10 @@
 import plotly.graph_objects as go
 import plotly.express as px
 
+
+def plot_refund_distribution(prod_info):
+    fig = px.pie(prod_info,
+                 names='product_id',
+                 values='refund_amount_usd',
+                 title='Refund Distribution by Product')
+    return fig
