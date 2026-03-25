0ffd6e840ca974eebd0958ad12f944c70655deaa
nidhiC14-BA <nidhi141200@gmail.com>
2026-03-25 19:06:23 +0530

    Finalised Dashboard Goal 3 & 4

--- visualization.py
+++ visualization.py
@@ -95,3 +95,100 @@ def create_user_pie_chart(user_counts):
     )
 
     return fig
+
+import plotly.express as px
+
+def plot_total_units_sold(product_sales):
+    fig = px.bar(
+        product_sales,
+        x=product_sales["product_id"].astype(str),
+        y="total_units_sold",
+        title="Total Units Sold per Product",
+        labels={
+            "x": "Product ID",
+            "total_units_sold": "Total Units Sold"
+        }
+    )
+
+    return fig
+
+def plot_trend(trend):
+    fig = px.line(
+        trend,
+        x='month',
+        y=['revenue', 'orders_count', 'refund_amount'],
+        title='Revenue, Orders, and Refund Trends Over Time',
+        markers=True
+    )
+
+    return fig
+
+def plot_refund_distribution(prod_info):
+    fig = px.pie(prod_info,
+                 names='product_id',
+                 values='refund_amount_usd',
+                 title='Refund Distribution by Product')
+    return fig
+def plot_order_gap_distribution(Order_gap):
+    fig = px.histogram(
+        Order_gap,
+        x=Order_gap,
+        nbins=20,
+        title="Distribution of Days Between First and Second Order"
+    )
+
+    return fig
+
+def plot_order_value_distribution(orders_with_type):
+    fig = px.violin(
+        orders_with_type,
+        x="user_type",
+        y="price_usd",
+        box=True,
+        points=False,
+        title="Order Value Distribution: New vs Repeat Customers"
+    )
+
+    fig.update_layout(
+        xaxis_title="Customer Type",
+        yaxis_title="Order Value (USD)"
+    )
+
+    return fig
+
+def plot_user_distribution(user_counts):
+    fig = px.pie(
+        values=user_counts.values,
+        names=user_counts.index,
+        title="New vs Repeat Users"
+    )
+
+    return fig
+def plot_repeat_purchase_rate(result):
+    fig = px.bar(
+        result,
+        x="product_name",
+        y="repeat_purchase_rate_%",
+        title="Actual Repeat Purchase Rate per Product"
+    )
+
+    fig.update_layout(
+        xaxis_title="Product Name",
+        yaxis_title="Repeat Purchase Rate (%)"
+    )
+
+    return fig
+def plot_refund_heatmap(heat):
+    fig = px.imshow(
+        heat,
+        text_auto=True,
+        color_continuous_scale='Blues',
+        title='Refund % per Product per Month'
+    )
+
+    fig.update_layout(
+        xaxis_title='Month',
+        yaxis_title='Product'
+    )
+
+    return fig
